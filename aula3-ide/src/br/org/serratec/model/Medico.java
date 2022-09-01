4820b2b543ff56cb908ef0317ae05fc7611440ae
PauloPetersen <paulo.g.petersen@hotmail.com>
2022-08-31 22:02:04 -0300

    POO - Aula3

---
+++ aula3-ide/src/br/org/serratec/model/Medico.java
@@ -0,0 +1,45 @@
+package br.org.serratec.model;
+
+public class Medico {
+	private Integer crm;
+	private String nome;
+	private Double salario;
+	private Double valorConsulta;
+	private static int contador;
+	
+	public Medico(Integer crm, String nome, Double salario, Double valorConsulta) {
+		super();
+		this.crm = crm;
+		this.nome = nome;
+		this.salario = salario;
+		this.valorConsulta = valorConsulta;
+		contador++;
+	}
+
+	public Integer getCrm() {
+		return crm;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public static int getContador() {
+		return contador;
+	}
+
+	public Double getValorConsulta() {
+		return valorConsulta;
+	}
+	
+	public void pagamentoDinheiro() {
+		salario += valorConsulta;
+	}
+	public void pagamentoPlanoDeSaude() {
+		salario = salario + valorConsulta * 0.70;
+	}
+}
---
+++ aula3-ide/src/br/org/serratec/model/Produto.java
@@ -0,0 +1,36 @@
+package br.org.serratec.model;
+
+public class Produto {
+	private Integer codigo;
+	private String descricao;
+	private Double valor;
+	//Este atributo pertence a classe (static)
+	private static int totalProdutos;
+	
+	public Produto(Integer codigo, String descricao, Double valor) {
+		super();
+		this.codigo = codigo;
+		this.descricao = descricao;
+		this.valor = valor;
+		totalProdutos+=1;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public static int getTotalProdutos() {
+		return totalProdutos;
+	}
+	
+	
+
+}
